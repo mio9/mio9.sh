4f1d2754832a02e1883d5210f6f7994f00d0a9be
mio9 <wonghoyeung1109@gmail.com>
2019-04-05 00:56:18 +0900

    adds back the fuck

---
+++ pages/now-you-fuck-off.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="test">
+    <span class="superlarge">GET OUT</span>
+<pre>                     /"\
+                    |\./|
+                    |   |
+                    |   |
+                    |>~<|
+                    |   |
+                 /'\|   |/'\..
+             /~\|   |   |   | \
+            |   =[@]=   |   |  \
+            |   |   |   |   |   \
+            | ~   ~   ~   ~ |`   )
+            |                   /
+             \                 /
+              \               /
+               \    _____    /
+                |--//''`\--|
+                | (( +==)) |
+                |--\_|_//--|</pre>
+  </div>
+</template>
+<script>
+export default {
+  name: 'FuckOff',
+  data(){
+    return{
+      
+    }
+  }
+}
+</script>
+<style>
+pre{
+  font-family: "Roboto Mono", Courier, monospace;
+}
+.superlarge{
+  font-size: 50px;
+  font-weight: 700;
+}
+</style>
+
