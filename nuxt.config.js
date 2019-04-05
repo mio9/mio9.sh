de5ad10d995d889d618d92100b689cfc6bc5a42a
mio9 <wonghoyeung1109@gmail.com>
2019-04-05 17:31:45 +0900

    fix incorrect icon desctiption, add theme color

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,10 +11,10 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: pkg.description },
+      { name: 'theme-color', content: '#202020'}
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-  {rel: 'shortcut icon', type: 'image/png', href:"/favicon.png"}]
+    link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }]
   },
 
   /*
